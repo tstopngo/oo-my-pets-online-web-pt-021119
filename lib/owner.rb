1440e56427e8186755f9ed9f860ece592c5f7697
Tracy Ngo <tngo1206@gmail.com>
2019-03-21 16:48:18 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,15 @@
 class Owner
   # code goes here
+  @@all = []
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @pets = {fishes: [], cats:[], dogs:[]}
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
 end
